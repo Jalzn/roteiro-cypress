5b52966408265230ca51c227f3225777795c4e52
Jalzn <jalmirfsjr@gmail.com>
2024-12-09 20:03:09 -0300

    Cria 3 novos tests e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,16 @@ describe('TODOMvc App', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
+      .should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
@@ -36,7 +36,7 @@ describe('TODOMvc App', () => {
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
@@ -68,4 +68,95 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  // Para essa task optei por nao criar novos seletores
+  // no index.html para manter o commit em apenas um arquivo
+  // e diversificar o modo de pesquisa dos elementos
+  // para aprender usar as classes dos elementos da DOM
+  // alem do metodo normal de data-cy
+
+  it('Verifica o numero de items faltantes', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('.todo-count')
+      .should('have.text', '1 item left')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click()
+
+    cy.get('.todo-count')
+      .should('have.text', '0 items left')
+  })
+
+  it('Marca todas as tarefas como completas usando o atalho na barra de pesquisa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+      .type('TP2 de Engenharia de Software{enter}')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('.toggle-all-label')
+      .click()
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3)
+      .first()
+      .should('have.text', 'TP2 de ES');
+  })
+
+  it('Limpa a lista de tarefas completas', () => {
+
+
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+      .type('TP2 de Engenharia de Software{enter}')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('.toggle-all-label')
+      .click()
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('.clear-completed')
+      .click()
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0)
+  })
+
 });
